6302448fc2c82b31606b19a3be13f921caa1b0f9
wutongli <femdomtong@outlook.com>
2019-09-20 11:33:55 +0800

    dj

---
+++ bank_Client/src/main/java/com/zl/bts/controller/TransactionRecordController.java
@@ -0,0 +1,31 @@
+package com.zl.bts.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zl.bts.pojo.Layui;
+import com.zl.bts.pojo.TransactionRecord;
+import com.zl.bts.service.impl.TransactionRecordServiceImpl;
+
+@Controller
+@RequestMapping("/trc")
+public class TransactionRecordController {
+	@Autowired
+	private TransactionRecordServiceImpl traservice;
+	//查询历史转账记录
+	@RequestMapping("queryFinshedRecord")
+	public Layui queryFinshedRecord(Integer page,Integer limit) {
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("endPage", page*limit);
+		map.put("beginPage", (page-1)*limit);
+		List<TransactionRecord> list = traservice.queryFinshed(map);
+		Integer totalCountFinshed = traservice.queryTotalCountFinshed();
+		
+		return  Layui.data(totalCountFinshed, list);
+	}
+}
--- bank_Client/src/main/java/com/zl/bts/dao/TransactionRecordDao.java
+++ bank_Client/src/main/java/com/zl/bts/dao/TransactionRecordDao.java
@@ -1,7 +1,20 @@
 package com.zl.bts.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import com.zl.bts.pojo.TransactionRecord;
 
 public interface TransactionRecordDao {
 	public void addTransactionRecord(TransactionRecord t);
+	
+	//查询历史转账记录
+	public List<TransactionRecord> queryFinshed(Map<String,Object> map);
+	//查询未完成转账记录
+	public List<TransactionRecord> queryUnFished(Map<String,Object> map);
+	
+	//查询历史转账记录总数据量
+	public Integer queryTotalCountFinshed();
+	// 查询未完成转账记录总数据量
+	public Integer queryTotalCountUnFinshed();
 }
